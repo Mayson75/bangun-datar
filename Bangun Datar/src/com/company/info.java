40debdce1c032cc0468d49ba9e80bbb393d8f998
Mayson75 <81522614+Mayson75@users.noreply.github.com>
2021-05-24 00:57:52 +0700

    Add files via upload

---
+++ Bangun Datar/src/com/company/info.java
@@ -0,0 +1,6 @@
+package com.company;
+
+abstract class info {
+    abstract void show_sudut();
+    abstract void show_info();
+}
---
+++ Bangun Datar/src/com/company/luas_keliling.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface luas_keliling {
+    abstract public double luas();
+    abstract public double keliling();
+}
